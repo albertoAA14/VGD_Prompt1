c59f3cae5e18c580f826821b0b37e2d4d78fd033
albertoAA14 <alberto451@stemcollegiate.ca>
2025-03-05 09:00:21 -0700

    Create mar 5.cs

---
+++ mar 5.cs
@@ -0,0 +1,11 @@
+Console.WriteLine(0.25F);
+Console.WriteLine('T');
+Console.WriteLine(2.625);
+Console.WriteLine(12.39816m);
+Console.WriteLine(true);
+Console.WriteLine(false);
+Console.WriteLine("123");
+Console.WriteLine(123);
+
+Console.WriteLine("true");
+Console.WriteLine(true);
